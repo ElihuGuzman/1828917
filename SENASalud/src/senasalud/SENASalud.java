48a9de36d3b827f1a98159d81f6544db99b2d6d3
ElihGuzman <sala@HOR-AULA501-017.unihorizonte.salas>
2019-10-29 15:52:20 -0500

    Mi primer commit

---
+++ SENASalud/src/senasalud/SENASalud.java
@@ -0,0 +1,19 @@
+
+package senasalud;
+
+import java.util.Scanner;
+
+public class SENASalud {
+
+    
+    public static void main(String[] args) {
+        
+        String Nomb;
+        Scanner nombre = new Scanner (System.in);
+        System.out.println("Indique su nombre");
+        Nomb = nombre.next();
+        System.out.println("Su nombre es: "+Nomb);
+        
+    }
+    
+}
