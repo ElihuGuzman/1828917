e8391b807df37306f0b3f913378b5bbcba3f2db9
ElihGuzman <sala@HOR-AULA501-017.unihorizonte.salas>
2019-10-29 16:40:33 -0500

    Mi segundo commit

--- SENASalud/src/senasalud/SENASalud.java
+++ SENASalud/src/senasalud/SENASalud.java
@@ -8,12 +8,16 @@ public class SENASalud {
     
     public static void main(String[] args) {
         
-        String Nomb;
+        String Nomb,Apel;
         Scanner nombre = new Scanner (System.in);
         System.out.println("Indique su nombre");
         Nomb = nombre.next();
         System.out.println("Su nombre es: "+Nomb);
         
+        Scanner apellido = new Scanner (System.in);
+        System.out.println("Indique su apellido");
+        Apel = apellido.next();
+        System.out.println("Su apellido es: "+Apel);
     }
     
 }
